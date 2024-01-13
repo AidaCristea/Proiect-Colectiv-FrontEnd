d28bbc2bf98d89a62daa43c972c3cf3096bac63b
911_Arba_Catalina <91603584+CatalinaArba@users.noreply.github.com>
2024-01-13 20:15:57 +0200

    bug fix

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
-import {User} from "../Entity/User";
 import {AuthServiceComponent} from "../../Services/auth-service/auth-service.component";
 
 @Component({
